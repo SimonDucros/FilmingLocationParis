fba36340d870b33d93f35084b7aabb4ac3f54ab8
Delphine <delphine.gesse@etu.emse.fr>
2022-12-04 12:40:14 +0100

    menu added, retrofit is not recognised, Mais activity still has book activity parts

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.ismin.android
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val SERVER_BASE_URL = "https://shootingLocations-gme.cleverapps.io"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val locations = ShootingLocation()
+    private val btnCreateBook: FloatingActionButton by lazy { findViewById(R.id.a_main_btn_create_book) }
+
+    val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(SERVER_BASE_URL)
+        .build()
+    val bookService = retrofit.create(ShootingLocationService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bookService.getAllBooks()
+            .enqueue(object : Callback<List<ShootingLocation>> {
+                override fun onResponse(
+                    call: Call<List<ShootingLocation>>,
+                    response: Response<List<ShootingLocation>>
+                ) {
+                    response.body()?.forEach { locations.addShootingLocation(it) }
+                    displayListFragment()
+                }
+                override fun onFailure(call: Call<List<ShootingLocation>>, t: Throwable) {
+                    Toast.makeText(applicationContext,"Cannot display books", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        btnCreateBook.setOnClickListener {
+            displayCreateBookFragment()
+        }
     }
+
+    private fun displayListFragment() {
+        val bookListFragment = ListFragment.newInstance(locations.getAllLocations())
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.a_main_frame_layout, bookListFragment)
+            .commit()
+    }
+
+    private fun displayAppInfoFragment() {
+        val appInfoFragment = AppInfoFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.a_main_frame_layout, appInfoFragment)
+            .commit()
+    }
+
+    private fun displayMapFragment() {
+/*        val mapFragment = MapFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.a_main_frame_layout, mapFragment)
+            .commit()
+        btnCreateBook.hide()
+
+ */
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_list -> {
+                displayListFragment()
+                true
+            }
+            R.id.menu_map -> {
+                displayMapFragment()
+                true
+            }
+            R.id.menu_info -> {
+                displayAppInfoFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBookCreated(shootLocation: ShootingLocation) {
+        bookService.createBook(shootLocation)
+            .enqueue(object : Callback<ShootingLocation> {
+                override fun onResponse(call: Call<ShootingLocation>,response: Response<ShootingLocation>) {
+                    response.body()?.let{locations.addBook(it)}
+                    displayListFragment()
+                }
+                override fun onFailure(call: Call<ShootingLocation>, t: Throwable) {
+                    Toast.makeText(applicationContext,"Cannot be added to favourites", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
 }
